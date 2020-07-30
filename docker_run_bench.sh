8f3036e68b6214f0e88dd19ecf78bcfd0cb58b8c
dragondriver <longjiquan1998@163.com>
2020-07-30 18:01:51 +0800

    update milvus

--- docker_run_bench.sh
+++ docker_run_bench.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-python run.py --dataset sift-128-euclidean --batch --definitions algos.yaml.faiss.8192
+#python run.py --dataset sift-128-euclidean --batch --definitions algos.yaml.faiss-ivf.8192
+#python run.py --dataset sift-128-euclidean --batch --definitions algos.yaml.faiss-ivf.4096
+#python run.py --dataset sift-128-euclidean --batch --definitions algos.yaml.faiss-ivf.2048
+#python run.py --dataset sift-128-euclidean --batch --definitions algos.yaml.faiss-ivf.1024
+#python run.py --dataset sift-128-euclidean --batch --definitions algos.yaml.faiss-ivf.512
+
+python run.py --dataset sift-128-euclidean --batch --definitions algos.yaml.milvus-ivf-flat.8192
+python run.py --dataset sift-128-euclidean --batch --definitions algos.yaml.milvus-ivf-flat.4096
+python run.py --dataset sift-128-euclidean --batch --definitions algos.yaml.milvus-ivf-flat.2048
+python run.py --dataset sift-128-euclidean --batch --definitions algos.yaml.milvus-ivf-flat.1024
+python run.py --dataset sift-128-euclidean --batch --definitions algos.yaml.milvus-ivf-flat.512
