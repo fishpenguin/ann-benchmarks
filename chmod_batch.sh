97c738608f2080d1283cc135d03a96bf25307a27
dragondriver <longjiquan1998@163.com>
2020-08-01 10:58:42 +0800

    update

--- chmod_batch.sh
+++ chmod_batch.sh
@@ -47,3 +47,59 @@ for fn in `ls`;do
     chmod a+w $fn
 done
 cd -
+
+cd results/sift-128-euclidean/10/faiss-ivf-sq-batch
+for fn in `ls`;do
+    echo $fn
+    chmod a+r $fn
+    chmod a+w $fn
+done
+cd -
+
+cd results/sift-128-euclidean/10/milvus-ivf-sq8-batch
+for fn in `ls`;do
+    echo $fn
+    chmod a+r $fn
+    chmod a+w $fn
+done
+cd -
+
+cd results/sift-128-euclidean/10/faiss-sq-batch
+for fn in `ls`;do
+    echo $fn
+    chmod a+r $fn
+    chmod a+w $fn
+done
+cd -
+
+cd results/sift-128-euclidean/10/faiss-hnsw-batch
+for fn in `ls`;do
+    echo $fn
+    chmod a+r $fn
+    chmod a+w $fn
+done
+cd -
+
+cd results/sift-128-euclidean/10/milvus-hnsw-batch
+for fn in `ls`;do
+    echo $fn
+    chmod a+r $fn
+    chmod a+w $fn
+done
+cd -
+
+cd results/sift-128-euclidean/10/hnswlib-batch
+for fn in `ls`;do
+    echo $fn
+    chmod a+r $fn
+    chmod a+w $fn
+done
+cd -
+
+cd results/sift-128-euclidean/10/sptag-batch
+for fn in `ls`;do
+    echo $fn
+    chmod a+r $fn
+    chmod a+w $fn
+done
+cd -
