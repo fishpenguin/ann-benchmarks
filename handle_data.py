36897e668594c1c0fbac72251fd9d99b87b90476
DragonDriver <longjiquan1998@163.com>
2020-08-06 11:49:44 +0800

    add gist

--- handle_data.py
+++ handle_data.py
@@ -4,6 +4,14 @@ import os
 import struct
 import numpy as np
 
+def ivecs_read(fname):
+    a = np.fromfile(fname, dtype='int32')
+    d = a[0]
+    return a.reshape(-1, d + 1)[:, 1:].copy()
+
+def fvecs_read(fname):
+    return ivecs_read(fname).view('float32')
+
 def bvecs_to_ndarray(bvecs_fn):
     with open(bvecs_fn, 'rb') as f:
         fsize = os.path.getsize(bvecs_fn)
